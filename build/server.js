edca1deac539cc6edea662cca7ccaaecdb4216dd
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-11 23:43:13 -0500

    Add CORS headers

--- build/server.js
+++ build/server.js
@@ -10,5 +10,10 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT || 4000}`);
 });
-app.use(express_1.default.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
 app.use("/", routes_1.default);
--- server.ts
+++ server.ts
@@ -9,6 +9,11 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT || 4000}`);
 });
 
-app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
 
 app.use("/", routes);

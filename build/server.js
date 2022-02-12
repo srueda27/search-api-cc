f20fd4ffd9ba0767d28e39d21585b3a74dad0d46
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-11 23:37:31 -0500

    Compile code

--- build/server.js
+++ build/server.js
@@ -4,13 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-var cors = require('cors');
 const routes_1 = __importDefault(require("./routes"));
 const app = express_1.default();
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT || 4000}`);
 });
-app.use(cors);
-app.options('*', cors());
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // Pass to next layer of middleware
+    next();
+});
 app.use("/", routes_1.default);
--- server.ts
+++ server.ts
@@ -1,5 +1,4 @@
 import express from "express";
-var cors = require('cors')
 import routes from "./routes";
 
 const app: express.Application = express();
@@ -10,7 +9,23 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT || 4000}`);
 });
 
-app.use(cors);
-app.options('*', cors());
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use("/", routes);

12533709c7c3437d5f7f4d8f571703ed5f59abef
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-13 17:47:08 -0500

    Add boilerplate for Test with Jest framework

---
+++ tests/persistenceService.test.js
@@ -0,0 +1,12 @@
+const { persistenceService } = require("../build/services/PersistenceService");
+
+test('Test Boilerplate', () => {
+
+});
+
+test('Should return an object', (done) => {
+  persistenceService.getPhotoById(204)
+    .then((result) => {
+      expect(typeof result).toBe(typeof {})
+    })
+})

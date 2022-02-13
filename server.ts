d0a87c66a4f475caf37715bafc834af52497c4ea
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-13 12:39:16 -0500

    CORS

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-type');
   next();
 });
 

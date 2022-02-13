4a5cb09252faf42eb96d2cb7814dd1d242004a77
Santiago Rueda Ortiz <srueda27@gmail.com>
2022-02-13 13:27:08 -0500

    Remove console.logs

--- build/routes/photos.js
+++ build/routes/photos.js
@@ -40,7 +40,6 @@ router.get('/title/:title', async function (request, response) {
 });
 router.post('/', async function (request, response) {
     console.log('POST Photos started');
-    console.log('request: ', request.body);
     const title = request.body.title;
     const photoUrl = request.body.photoUrl;
     const description = request.body.description;
